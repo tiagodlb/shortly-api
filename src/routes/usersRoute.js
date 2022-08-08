8fd7edc578f619553cb58bf220158b60850183dd
Tiago <tigolimab223@gmail.com>
2022-08-08 18:07:13 -0300

    bugfix

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
-import { postSignUp,postSignIn } from "../controllers/authController.js";
-import { userExists, emailExists, ValidateLoginCompatibility} from "../middlewares/usersMiddleware.js";
+import { postSignUp, postSignIn } from "../controllers/authController.js";
+import { getUserMe } from "../controllers/userController.js";
+import {
+  userExists,
+  emailExists,
+  ValidateLoginCompatibility,
+} from "../middlewares/usersMiddleware.js";
 import { ValidateSession } from "../middlewares/sessionMiddleware.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", emailExists, postSignUp);
 userRouter.post("/signin", userExists, ValidateLoginCompatibility, postSignIn);
-userRouter.get('/users/me', ValidateSession, getUsersMe);
+userRouter.get("/users/me", ValidateSession, getUserMe);
 
 export default userRouter;
