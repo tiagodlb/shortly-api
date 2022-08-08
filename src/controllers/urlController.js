d969d56e26aa950a6be9732a41ad65149ee28db9
Tiago <tigolimab223@gmail.com>
2022-08-08 14:13:56 -0300

    get User Me controller

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -37,3 +37,44 @@ export async function postShortUrl(req, res) {
     return res.send("Error when you're shorting a link").status(500);
   }
 }
+
+export async function getUrlId(req, res) {
+  try {
+    const shortUrl = res.locals.shortUrl;
+
+    res.status(200).send(shortUrl);
+  } catch (error) {
+    return res.send("Error when you're getting a short url").status(500);
+  }
+}
+
+export async function getOpenUrl(req, res) {
+  try {
+    const url = res.locals.url;
+    const id = res.locals.id;
+    const newCount = res.locals.visitsCount + 1;
+
+    await connection.query(
+      `
+        UPDATE urls SET "visitsCount"=$1 WHERE id=$2
+        `[(newCount, id)]
+    );
+    res.redirect(url);
+  } catch (error) {
+    return res.send("Error when you're opening a url").status(500);
+  }
+}
+
+export async function deleteUrl(req, res) {
+  const id = parseInt(req.params.id);
+  const authorization = req.headers.authorization;
+  const token = authorization?.replace("Bearer", "");
+
+  try {
+    await connection.query(`DELETE FROM urls WHERE id=$1`, [id]);
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res.send("Error when you're deleting a url").status(500);
+  }
+}
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,29 @@
 import connection from "../dbStrategy/postgres.js";
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+
+export async function getUserMe(req,res){
+  const { user } = res.locals;
+  try {
+    const {rows: queryData } = await connection.query(`
+    SELECT id, "shortUrl", url, "visitCount" FROM "shortUrls" WHERE "userId" = $1;
+    `, [user.id]);
+    
+    let totalVisits = 0;
+    queryData.map(e => {
+      totalVisits = e.visitsCount + totalVisits
+    });
+
+    const userData = {
+      id: user.id,
+      name: user.name,
+      visitsCount: totalVisits,
+      shortUrls: queryData
+    }
+
+    return res.status(200).send(userData);
+    
+  } catch (error) {
+    return res.send("Error when you're getting your own info").status(500);
+  }
+}
