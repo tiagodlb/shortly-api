3cbbf4ae603f4f156ab58b864fc608dea97f7180
Tiago <tigolimab223@gmail.com>
2022-08-08 18:18:10 -0300

    dump.sql added

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -1,11 +1,12 @@
 import joi from "joi";
 
 export const signUpSchema = joi.object({
-  name: joi.string().required().length(100),
+  name: joi.string().required().max(30),
   email: joi.string().email().required(),
   password: joi
     .string()
-    .pattern(new RegExp(/[a-zA-Z0-9!@#$%^&*()\-__+.]{11,}/))
+    .pattern(new RegExp(/[a-zA-Z0-9!@#$%^&*()\-__+.]{30,}/))
+    .max(30)
     .required(),
   confirmPassword: joi.string().valid(joi.ref("password")).required(),
 });
